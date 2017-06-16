224e9c0e4a64706f0fc9a82960ba308d2956c7ce
Alex <wersion007@mail.ru>
2017-06-16 21:02:26 +0300

    fix

--- src/main/java/ru/oav/entity/HhDepartment.java
+++ src/main/java/ru/oav/entity/HhDepartment.java
@@ -3,24 +3,6 @@ package ru.oav.entity;
 /**
  * Created by antonorlov on 16/06/2017.
  */
-public class HhDepartment {
+public class HhDepartment  extends HhIdNameEntiry{
 
-    private int id;
-    private String name;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }

6ddff99be4a24f996a59ffd5a49cfaf085326b12
Alex <wersion007@mail.ru>
2017-10-18 22:01:34 +0300

    Добавление комментариев в пакету formatvacancy Добавление синглтона

---
+++ src/main/java/ru/af/formatvacancy/PropertyHolder.java
@@ -0,0 +1,65 @@
+package ru.af.formatvacancy;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Получение и хранение данных из application.properties
+ */
+public class PropertyHolder {
+
+    public final String PATHFILE;
+    public final String DB_URL;
+    public  static  final String USER;
+    public static final String PASSWORD;
+    public static final String MODE;
+    private static PropertyHolder instance;
+
+    private PropertyHolder() {
+        // инициализация свойств-констант
+        Properties properties = loadProperties();
+        PATHFILE = properties.getProperty("filepath");
+        DB_URL = properties.getProperty("dburl");
+        USER = properties.getProperty("user");
+        PASSWORD = properties.getProperty("password");
+        MODE = properties.getProperty("hh.mode");
+    }
+
+    /**
+     * Дает создание единственнго экзамляра класса PropertyHolder
+     * @return PropertyHolder
+     */
+
+    public static PropertyHolder getInstance() {
+        if (instance == null) {
+            instance = new PropertyHolder();
+        }
+        return instance;
+    }
+
+    /**
+     * Загружает данные из файла application.properties со свойствами
+     * @return prop Properties
+     */
+    private static Properties loadProperties() {
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
+            input = new FileInputStream("src/main/resources/application.properties");
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return prop;
+    }
+}
